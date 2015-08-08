e6ca9b31f41118ff14db91cfa4dd500f4fbbafd4
Krittika DSilva <krittika.dsilva@gmail.com>
2015-08-07 21:53:01 -0700

    Added files

---
+++ scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dsilvaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dsilvaApp
+ */
+angular.module('dsilvaApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ scripts/controllers/publications.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dsilvaApp.controller:AboutCtrl
+ * @description
+ * # PublicationsCtrl
+ * Controller of the dsilvaApp
+ */
+angular.module('dsilvaApp')
+  .controller('PublicationsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
