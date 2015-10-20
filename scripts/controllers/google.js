2ca4102e20a18a9c452d4a7bc24cefca0c8498ec
Krittika DSilva <krittika.dsilva@gmail.com>
2015-10-20 07:07:11 -0700

    Adding google page.

---
+++ scripts/controllers/google.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dsilvaApp.controller:GoogleCtrl
+ * @description
+ * # GoogleCtrl
+ * Controller of the dsilvaApp
+ */
+angular.module('dsilvaApp')
+  .controller('GoogleCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
